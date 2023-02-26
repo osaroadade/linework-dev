41d4c0c793a296f4eeec685b17386abffb41ebbf
osaroadade <adadechuks@gmail.com>
2023-02-26 15:52:48 +0100

    Created header and footer template.

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
         dir: {
             input: "src",
             output: "public",
